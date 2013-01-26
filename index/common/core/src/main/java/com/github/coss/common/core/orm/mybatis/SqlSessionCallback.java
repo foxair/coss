72da0bd8ecf812cbee0f6fd864a97f9fae1ea1ba
foxair <azheng270@gmail.com>
2013-01-26 23:54:59 +0800

    clean index dir

--- index/common/core/src/main/java/com/github/coss/common/core/orm/mybatis/SqlSessionCallback.java
+++
@@ -1,7 +0,0 @@
-package com.github.coss.common.core.orm.mybatis;
-
-import org.apache.ibatis.session.SqlSession;
-
-public interface SqlSessionCallback {
-    public Object doInSqlSession(SqlSession session);
-}
